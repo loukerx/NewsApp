f94393fc12d0ddfea00006346dfee01b6a3b3301
Yin Hua <loukerx@gmail.com>
2025-07-12 11:27:16 +1000

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Yin Hua on 12/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
